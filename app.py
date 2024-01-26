5eaa11595320ed5f4a34a904ba2aa08c63cd3d79
Jawid-Yari <jawidyari@gmail.com>
2024-01-26 15:01:08 +0100

    installed required packages

--- app.py
+++ app.py
@@ -1,64 +1,99 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import base64
+import io
+from PIL import Image 
+import keras 
+from keras import backend as K 
+from keras.models import Sequential
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
 import numpy as np
-from flask_migrate import Migrate, upgrade
 
 app = Flask(__name__)
 
 # Load your trained model
 model_path = r"C:\Users\46722\Downloads\mobilenet_v2_weights_tf_dim_ordering_tf_kernels_1.0_224_no_top.h5"
 
-@app.route('/', methods=['GET'])
-def index():
-    # Render the main page
-    return render_template('test.html')
+def get_model():
+    global model
+    model = load_model('best_race_model.h5')
+    print('*Model loaded*')
 
+def preprocessing_image(image, target_size):
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    image = image.resize(taget_size)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+    
+    return image
 
-@app.route('/about', methods=['GET'])
-def about():
-    # Render the about page
-    return render_template('about.html')
-
-@app.route('/contact', methods=['GET'])
-def contact():
-    # Render the contact page
-    return render_template('contact.html')
-
-@app.route('/upload')
-def upload():
-    return render_template('upload_form.html')
-
+print('*loading keras model...')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Check if a file is received
-    file = request.files['file']
-    if file.filename == '':
+    image = request.files['image']
+    if image.filename == '':
         return 'No selected file'
+    decoded = base64.b64decode(image)
+    image = Image.open(io.BytesIO(decoded))
+    processed_image = preprocessing_image(image, target_size =(224, 224))
 
-    if file:
-        # Preprocess the file here
-        img = image.load_img(file, target_size=(224, 224))
-        img = image.img_to_array(img)
-        img = np.expand_dims(img, axis=0)
+    prediction = model.predict(processed_image).tolist()
 
-        # Make prediction
-        prediction = model.predict(img)
+    response = {
+        'prediction': {
+            'Black' : prediction[0][0],
+            'East_Asian': prediction[0][1]
+        }
+    }
+    return jsonify(response)
 
-        # You'll need to translate the prediction to your desired output
-        # For example, converting numerical prediction to labels
 
-        # Format the response
-        age, gender, race = parse_prediction(prediction)
 
-        return render_template('result.html', age=age, gender=gender, race=race)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+
+
+
+
+
+# @app.route('/', methods=['GET'])
+# def index():
+#     # Render the main page
+#     return render_template('home.html')
+
+
+# @app.route('/about', methods=['GET'])
+# def about():
+#     # Render the about page
+#     return render_template('about.html')
+
+# @app.route('/contact', methods=['GET'])
+# def contact():
+#     # Render the contact page
+#     return render_template('contact.html')
+
+# @app.route('/upload')
+# def upload():
+#     return render_template('upload_form.html')
+
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     # Check if a file is received
+#     file = request.files['file']
+#     if file.filename == '':
+#         return 'No selected file'
+
+
 
 
 
 if __name__  == "__main__":
-    with app.app_context():
-        upgrade()
+    app.run(debug=True, port = 5001)
